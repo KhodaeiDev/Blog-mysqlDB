cfc20009920e3f6e18553317796b5744a1aa0cb3
khodaeidev <khodaei.dev@gmail.com>
2024-09-15 14:57:45 +0330

    Dev Start Server and connect to Db

--- src/server.js
+++ src/server.js
@@ -1,1 +1,18 @@
-require("./db");
+const app = require("./app");
+const db = require("./db");
+const configs = require("./config");
+
+async function startServer() {
+  try {
+    await db.getConnection();
+
+    app.listen(configs.port, () => {
+      console.log(`Server Running on Port ${configs.port}`);
+    });
+  } catch (err) {
+    console.log("err ->", err);
+    await db.end();
+  }
+}
+
+startServer();
