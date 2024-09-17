eda58846928514fe0d8cea8e074bedffa87b8da8
khodaeidev <khodaei.dev@gmail.com>
2024-09-17 12:42:46 +0330

    Dev tags Repositories

---
+++ src/repositories/tags.js
@@ -0,0 +1,59 @@
+const db = require("./../db");
+
+const create = async (title) => {
+  try {
+    const query = " INSERT INTO tags (title) VALUES (?)";
+
+    const insertTag = await db.execute(query, [title]);
+
+    const userQuery = "SELECT * FROM tags WHERE id =?";
+    const [tag] = await db.execute(userQuery, [insertTag.insertId]);
+
+    return tag[0];
+  } catch (err) {
+    throw err;
+  }
+};
+
+const findByTitle = async (title) => {
+  try {
+    const query = " SELECT * FROM tags where title = ?";
+
+    const tag = await db.execute(query, [title]);
+
+    return tag[0];
+  } catch (err) {
+    throw err;
+  }
+};
+
+const findAll = async () => {
+  try {
+    const query = " SELECT * FROM tags";
+
+    const tag = await db.execute(query);
+
+    return tag[0];
+  } catch (err) {
+    throw err;
+  }
+};
+
+const remove = async (id) => {
+  try {
+    const query = " DELETE FROM tags WHERE id = ?";
+
+    const removeTag = await db.execute(query, [id]);
+
+    return removeTag[0];
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = {
+  create,
+  findByTitle,
+  findAll,
+  remove,
+};
