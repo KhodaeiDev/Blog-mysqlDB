4320db7ba2e41aa1f9b826f57e58a64016afeafd
khodaeidev <khodaei.dev@gmail.com>
2024-09-18 12:14:17 +0330

    Add route Protections to tags create and remove

--- src/routes/tags.js
+++ src/routes/tags.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const controller = require("./../controllers/tags");
+const auth = require("../middleware/auth");
+const isAdmin = require("../middleware/isAdmin");
 
 const router = express.Router();
 
-router.route("/").get(controller.getAll).post(controller.create);
-router.route("/remove/:id").delete(controller.remove);
+router.route("/").get(controller.getAll).post(auth, isAdmin, controller.create);
+router.route("/remove/:id").delete(auth, isAdmin, controller.remove);
 
 module.exports = router;
