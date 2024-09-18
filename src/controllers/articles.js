93c85d344dc06d6b2b2ff5f587b6069ba2b92054
khodaeidev <khodaei.dev@gmail.com>
2024-09-18 14:31:27 +0330

    Dev articles Routes

---
+++ src/controllers/articles.js
@@ -0,0 +1,31 @@
+exports.getAll = async (req, res, next) => {
+  try {
+    // Coding
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.create = async (req, res, next) => {
+  try {
+    // Coding
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getBySlug = async (req, res, next) => {
+  try {
+    // Coding
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.remove = async (req, res, next) => {
+  try {
+    // Coding
+  } catch (err) {
+    next(err);
+  }
+};
---
+++ src/routes/articles.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const controller = require("./../controllers/articles");
+
+const router = express.Router();
+
+router.route("/").get(controller.getAll).post(controller.create);
+
+router.route("/:slug").get(controller.getBySlug);
+
+router.route("/remove/:id").post(controller.remove);
+
+module.exports = router;
