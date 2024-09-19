34cef635e75c3942ba99d9d0c4a14a663426cb5f
khodaeidev <khodaei.dev@gmail.com>
2024-09-19 11:24:01 +0330

    Dev uploader for uploading articles cover

--- src/routes/articles.js
+++ src/routes/articles.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const controller = require("./../controllers/articles");
+const path = require("path");
+const { multerStorage } = require("./../middleware/uploader");
 
 const router = express.Router();
 
-router.route("/").get(controller.getAll).post(controller.create);
+const upload = multerStorage(path.join(__dirname, "../../public/images/cover"));
+
+router
+  .route("/")
+  .get(controller.getAll)
+  .post(upload.single("cover"), controller.create);
 
 router.route("/:slug").get(controller.getBySlug);
 
